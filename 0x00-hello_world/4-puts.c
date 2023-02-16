172f6c70df8e04dccbc78cc24b7b3f5bef743809
Jason <kyleskaleb56@gmail.com>
2023-02-16 15:15:11 -0800

    4-puts.c created, i dont understand betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - function of project (prints sentence wrapped in double quotes)
+ *
+ * Return: return 0 everytime
+ */
+int main(void)
+{
+
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
+        puts(str);
+	return (0);
+}
