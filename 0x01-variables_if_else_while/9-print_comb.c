99601f27f9dec8034518943095a4d9689edc2515
Jason <kyleskaleb56@gmail.com>
2023-02-17 14:30:29 -0800

    9-print_comb.c created

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - main function of my program (entry point)
+ *
+ * Return: always return 0 = success
+ */
+int main(void)
+{
+	char n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		if (n == '9')
+		{
+			break;
+		}
+		putchar(',');
+		putchar(' ');
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
