c013228d72ce298428bafc4b2322e68f27799983
Jason <kyleskaleb56@gmail.com>
2023-02-17 13:17:25 -0800

    5-print_numbers.c created

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main function of the program(entry point)
+ *
+ * Return: always return 0 = success
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
