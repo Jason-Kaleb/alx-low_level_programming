6b3a6dac75c2e6a3b7f562d4595ab5f4b7fd4467
Jason <kyleskaleb56@gmail.com>
2023-02-18 09:22:39 -0800

    102-print_comb5.c edited

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,7 +2,7 @@
 /**
  * main - main function of my program(entry point)
  *
- * description: prints all possible combinations of two two-digit numbers
+ * description: prints all possible combinations of two two-digit numbers that do not repeat
  * Return: always return 0 = success
  */
 int main(void)
@@ -17,16 +17,20 @@ int main(void)
 			{
 				for (dig = 49; dig < 58; dig++)
 				{
+					if (n > numb)
+					{
+						continue;
+					}
+					if (num >= dig)
+					{
+						continue;
+					}
 					putchar(n);
 					putchar(num);
 					putchar(' ');
 					putchar(numb);
 					putchar(dig);
-					if (n == 58 && num == 57)
-					{
-						break;
-					}
-					if (numb == 58 && dig == 58)
+					if (n == 57 && num == 56)
 					{
 						break;
 					}
