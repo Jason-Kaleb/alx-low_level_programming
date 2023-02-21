f0541cb9ee46e6bbc6cea8cea7b5d2f14823fe94
Jason <kyleskaleb56@gmail.com>
2023-02-21 14:26:34 -0800

    0-putchar.c created

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - main function of my program (entry point)
+ *
+ *
+ * description: prints a word using function _putchar
+ * Return: always return 0 = success
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
