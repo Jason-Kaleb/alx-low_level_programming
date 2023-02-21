c0f484c396f782e35ad73a2c9b9b17b06803fa69
Jason <kyleskaleb56@gmail.com>
2023-02-21 15:39:33 -0800

    2-print_alphabet_x10.c edited

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,16 +8,16 @@
 void print_alphabet_x10(void)
 {
 	int a = 97;
-	int j = 0; 
+	int j = 0;
 
 	while (j <= 10)
 	{
+		j++;
 		while (a <= 122)
 		{
 			_putchar(a);
 			a++;
 		}
 		_putchar('\n');
-		j++;
 	}
 }
