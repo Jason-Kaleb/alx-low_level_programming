83bd0da4f2659a915172fbb8369ebf8678f27987
Jason <kyleskaleb56@gmail.com>
2023-02-21 15:37:47 -0800

    2-print_alphabet_x10.c created

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet in lower case 10 times
+ *
+ * Return: always 0 = success
+ */
+void print_alphabet_x10(void)
+{
+	int a = 97;
+	int j = 0; 
+
+	while (j <= 10)
+	{
+		while (a <= 122)
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar('\n');
+		j++;
+	}
+}
