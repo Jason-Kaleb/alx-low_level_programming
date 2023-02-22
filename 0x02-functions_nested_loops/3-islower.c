b327c29ab1eb842872bee607b1178917068dd5fb
Jason <kyleskaleb56@gmail.com>
2023-02-22 07:21:31 -0800

    betty doesnt like me

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,32 +1,18 @@
 #include "main.h"
 #include <ctype.h>
-/*
- * _islower - checks for lowercase character
- *
- * Return: always 0 = success
+/**
+ * _islower - checks if a letter is a lowercase letter
+ * @c: letter input
+ * Return: 1 if c = lowercase and 0 if else
  */
 int _islower(int c)
 {
 	if (islower(c))
 	{
-		return 1;
+		return (1);
 	}
 	else
 	{
-		return 0;
+		return (0);
 	}
-}
-
-int main(void)
-{
-	int r;
-
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
 }
