1633e5e944580ea999799cd168af84016971808b
Jason <kyleskaleb56@gmail.com>
2023-02-23 06:34:44 -0800

    0-main.c created

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - tests an int whether it is postive or negative
+ * Return: 0 always = success
+ */
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
