758dd323770776fef0537479d9e79b7c298a107e
Jason <kyleskaleb56@gmail.com>
2023-02-23 06:40:36 -0800

    0-main.c edited

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,15 @@
 #include "main.h"
+
 /**
  * main - tests an int whether it is postive or negative
  * Return: 0 always = success
  */
+
 int main(void)
 {
 	int i;
 
-	i = 98;
+	i = 8;
 	positive_or_negative(i);
 
 	return (0);
