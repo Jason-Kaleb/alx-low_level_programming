a71eb39d7a6d55da3a3b6a6b00252fbb8bda1273
Jason <kyleskaleb56@gmail.com>
2023-02-23 15:44:49 -0800

    7-diagonal.c created

---
+++ 0x04-more_functions_nested_loops/7-diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints the character \
+ * @n: number of times to be printed
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int a, b;
+
+	for (a = 0; a < n; a++)
+	{
+		for (b = 0; b < a; b++)
+		{
+			_putchar(32);
+			if (n <= 0)
+			{
+				_putchar('\n');
+			}
+		}
+		_putchar(92);
+		_putchar('\n');
+	}
+}
