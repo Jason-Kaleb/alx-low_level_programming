839fdda505967288fff14d56ad8c22874b1da1ed
Jason <kyleskaleb56@gmail.com>
2023-02-23 14:40:10 -0800

    4-print_most_numbers.c created

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers 0 - 9
+ * but not 2 and 4
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int a = 48;
+
+	while (a < 58)
+	{
+		if (a == 53 && a == 55)
+		{
+			continue;
+		}
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
