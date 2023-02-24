f878a005d1cf4502ea446ca8405b86a062caddde
Jason <kyleskaleb56@gmail.com>
2023-02-24 06:25:01 -0800

    9-fizz_buzz.c created smoothly

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 0-100
+ * but multiples of 3 prints fizz
+ * mulitiples of 5 prints buzz
+ * multiples of both prints fizzbuzz
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (a % 3 == 0 && a % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", a);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
