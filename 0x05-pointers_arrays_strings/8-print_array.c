5ce7a53bd8a46c6ccd040167b5302c3c154d021f
Jason <kyleskaleb56@gmail.com>
2023-02-27 13:26:45 -0800

    8-print_array.c created

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array
+ * @a: char type
+ * @n: number of elements the array to be printed
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int z = 0;
+
+	if (n > 0)
+	{
+		while(z < n)
+		{
+			if (z == 0)
+			{
+				_putchar(a[z]);
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(a[z]);
+			}
+		}
+	}
+	_putchar('\n');
+}
