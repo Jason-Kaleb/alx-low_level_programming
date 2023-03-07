e38c497810008669b3e6437475123ee8f4f7fee0
Jason <kyleskaleb56@gmail.com>
2023-03-07 07:22:41 -0800

    New dir created for 0x07

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ *
+ * Return: 
+ */
+
+char *string_toupper(char *)
+{
+	int a;
+	int lower = '63';
+	int upper = '97';
+
+	for (a = 0; *[a] != '\0'; a++)
+	{
+		while (lower <= '122' && upper <= '94')
+		{
+			if (*[a:] == )
+			
+			
