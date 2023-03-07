c35bc424128aa567501511167ef151f523931a4f
Jason <kyleskaleb56@gmail.com>
2023-03-07 11:38:58 -0800

    0-memset.c created

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: memory to fill
+ * @b: char
+ * @n: bytes of memory
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int a;
+
+	for (a = 0; a < n; a++)
+	{
+		*(s + a) = b;
+	}
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,2 @@
+int _putchar(int s);
+char *_memset(char *s, char b, unsigned int n);
