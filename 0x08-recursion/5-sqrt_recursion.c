0224b9667760c54392ac7b4a4d0040cbb21a4500
Jason <kyleskaleb56@gmail.com>
2023-03-09 09:34:02 -0800

    5-sqrt_recursion.c edited

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -40,17 +40,3 @@ int _sqrt_recursion(int n)
 
 	return (_sqrt(n, 1));
 }
-
-int main(void)
-{
-	int r;
-
-	r = _sqrt_recursion(1);
-	printf("%d\n", r);
-	r = _sqrt_recursion(1024);
-	printf("%d\n", r);
-	r = _sqrt_recursion(-1);
-	printf("%d\n", r);
-
-	return (0);
-}
