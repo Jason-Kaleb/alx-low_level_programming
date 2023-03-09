06829c5f1ceef5e3c1f2daaaf3f6445ba7652bb5
Jason <kyleskaleb56@gmail.com>
2023-03-09 10:03:20 -0800

    6-is_prime_number.c edited

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
- *  is_prime_number - checks whether a number is a 
- *  prime number or not
- *  @n: integer to be checked
- *  Return: 1 if integer isa prime number
- *  0 if else
+ * is_prime_number - checks whether a number is a
+ * prime number or not
+ * @n: integer to be checked
+ * Return: 1 if integer isa prime number
+ * 0 if else
  */
 
 int is_prime_number(int n)
@@ -19,7 +19,7 @@ int is_prime_number(int n)
 	{
 		return (1);
 	}
-	if ((n % 2 == 0) || (n % 3 == 0) || (n % 5 == 0))
+	if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0)
 	{
 		return (0);
 	}
@@ -33,28 +33,4 @@ int is_prime_number(int n)
 	}
 
 	return (is_prime_number(n));
-}
-
-int main(void)
-{
-	int r;
-
-	r = is_prime_number(1);
-	printf("%d\n", r);
-	r = is_prime_number(1024);
-	printf("%d\n", r);
-	r = is_prime_number(16);
-	printf("%d\n", r);
-	r = is_prime_number(17);
-	printf("%d\n", r);
-	r = is_prime_number(25);
-	printf("%d\n", r);
-	r = is_prime_number(-1);
-	printf("%d\n", r);
-	r = is_prime_number(113);
-	printf("%d\n", r);
-	r = is_prime_number(7919);
-	printf("%d\n", r);
-
-	return (0);
 }
